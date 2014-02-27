92dba8c3a58fadac110c692ece1646f5b2122f0d
Michael Xavier <michael@michaelxavier.net>
2014-02-27 10:36:41 -0800

    Work around bug in documenting splatted paths

--- lib/grape-swagger.rb
+++ lib/grape-swagger.rb
@@ -14,7 +14,8 @@ module Grape
 
         @combined_routes = {}
         routes.each do |route|
-          resource = route.route_path.split(route.route_prefix).last.match('\/(\w*?)[\.\/\(]').captures.first
+          match = route.route_path.split(route.route_prefix).last.match('\/(\w*?)[\.\/\(]')
+          resource = match ? match.captures.first : ""
           next if resource.empty?
           resource.downcase!
           @combined_routes[resource] ||= []
